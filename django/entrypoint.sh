4d892eae9cfa28075512f7204e5050ad76e3f1f6
magarets <alswn81616@gmail.com>
2023-09-13 07:33:32 +0000

    api v1

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-python manage.py makemigrations
+python manage.py makemigrations TeirenSIEM
 python manage.py migrate --no-input
-echo "migrate success"
+
+python manage.py makemigrations api
+python manage.py migrate --no-input
+
 python manage.py collectstatic --no-input
 python manage.py createsuperuser --noinput --username=yoonan --email=yoonan@teiren.io --password=cute428!
+
 gunicorn service.wsgi:application --bind 0.0.0.0:8000
