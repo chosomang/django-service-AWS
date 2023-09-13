e8be3e4f473c64247514bed71e6b8c759bc4cdb3
magarets <alswn81616@gmail.com>
2023-09-13 07:33:07 +0000

    api modules

--- django/api/urls.py
+++ django/api/urls.py
@@ -1,10 +1,20 @@
-
 from django.urls import path, include
+from rest_framework import routers
 from api import views
+from .views import create_cypher
+from .views import CypherListCreateView
+from .views import CypherViewSet
+from rest_framework.routers import DefaultRouter
 
-# Views
+router = DefaultRouter()
+router.register(r'cypher', CypherViewSet)
+
+
 urlpatterns = [
     # Dashboard
     path('', views.index),
-    path('v1/', views.version)
+    path('cypher/', views.cypher, name='cypher_field'),
+    path('create/', create_cypher, name='create_cypher'),
+    path('', include(router.urls))
+    # path('create/', CypherListCreateView.as_view(), name='cypher_create')
 ]
--- django/api/views.py
+++ django/api/views.py
@@ -1,12 +1,42 @@
-from django.shortcuts import render, HttpResponse
-from django.urls import reverse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import JsonResponse, HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import CypherSerializer
 
+API_VERSION = 'v1'
 # api index
 def index(request):
     return render(request, "api/index.html")
 
 def version(request):
     return render(request, "api/v1.html")
+
+def cypher(request):
+    if request.method == 'POST':
+        cypher = request.POST.get('cypher', '')
+        return render(request, 'api/{}/cypher.html'.format(API_VERSION), {'cypher': cypher})
+    else: # GET
+        return render(request, 'api/{}/cypher.html'.format(API_VERSION))
+
+from rest_framework import generics
+from rest_framework import viewsets
+from .models import Cypher
+from .serializers import CypherSerializer
+
+class CypherListCreateView(generics.ListCreateAPIView):
+    queryset = Cypher.objects.all()
+    serializer_class = CypherSerializer
+    
+
+class CypherViewSet(viewsets.ModelViewSet):
+    queryset = Cypher.objects.all()
+    serializer_class = CypherSerializer
+
+
+@api_view(['POST'])
+def create_cypher(request):
+    serializer = CypherSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
