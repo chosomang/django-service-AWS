b807abc7d8f921ccd7b006b9e9a1f576b14d5225
magarets <alswn81616@gmail.com>
2023-09-13 07:34:04 +0000

    Added REST api DRF setting

--- django/service/settings.py
+++ django/service/settings.py
@@ -28,17 +28,32 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# REST API DRF SETTINGS #
+from rest_framework import permissions
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+}
+
+
 
 # Application definition
 
 INSTALLED_APPS = [
     'TeirenSIEM',
+    'api',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
