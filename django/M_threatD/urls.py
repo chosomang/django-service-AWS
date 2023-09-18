8215957f7ccda5820a32f893bae62a9bedd6e6fc
magarets <alswn81616@gmail.com>
2023-09-18 07:47:28 +0000

    Added modal functions

--- django/M_threatD/urls.py
+++ django/M_threatD/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from M_threatD import views
+from M_threatD import modals
 
 urlpatterns = [
     ## alert
@@ -8,4 +9,8 @@ urlpatterns = [
     path('rules/<cloud_type>/', views.rules_view),
     ## visuals
     path('visuals/<cloud_type>/', views.visuals_view),
+    # Detail Modal
+    path('rules/<rule_type>/details/', modals.rule_details),
+    # Edit Modal
+    path('rules/<rule_type>/edit/', modals.rule_edit_modal)
 ]
