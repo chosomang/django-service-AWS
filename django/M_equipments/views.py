88b988da1ec9650bebb6e0a207e3924046a1a80e
magaretz <alswn81616@gmail.com>
2023-11-08 15:20:09 +0000

    feat: Create container trigger views file

--- django/M_equipments/views.py
+++ django/M_equipments/views.py
@@ -7,6 +7,7 @@ from .src import integration
 @login_required
 def integration_view(request):
     context = integration.list_integration()
+    
     return render(request, "M_equipment/integration.html", context)
 
 @login_required
@@ -15,21 +16,26 @@ def integration_page(request, equipment):
         if request.method == 'POST':
             data = dict(request.POST.items())
             context = integration.delete_integration(data)
+            
             return HttpResponse(context)
+        
     return render(request, f"M_equipment/{equipment}.html")
 
 def integration_check_ajax(request, equipment):
     if request.method == 'POST':
         context = integration.integration_check(request)
+        
         return JsonResponse(context)
 
 def integration_insert_ajax(request, equipment):
     if request.method == 'POST':
         context = integration.integration_insert(request)
+        
         return HttpResponse(context)
 
 def integration_collection_ajax(request, equipment):
     if request.method == 'POST':
         data = dict(request.POST.items())
-        context = integration.collection_on_off(data, equipment)
-        return HttpResponse(context)
+        context = integration.container_trigger(data, equipment)
+        
+        return JsonResponse(context)
