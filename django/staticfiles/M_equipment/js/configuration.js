8d854b8367e479a33d3e2797a174c87a23e88d58
chosomang <chosomang12@gmail.com>
2023-11-18 23:34:51 +0900

    feat: Created js file for configuration page

--- django/staticfiles/M_equipment/js/configuration.js
+++ django/staticfiles/M_equipment/js/configuration.js
@@ -52,5 +52,44 @@ $('#delete_complete').on('click', function(event) {
     location.reload();
 });
 
+function container_trigger(e, type, access_key, secret_key, region_name){
+    console.log(access_key, secret_key, region_name)
+    var on_off = 0
+    var input = $(e.parentNode).find('input.on_off')[0]
+    if (input.value != 1){
+        on_off = 1
+    }
+    input.value = on_off
+    $.ajax({
+        url:`${type}/collection/`,
+        headers:{
+            'X-CSRFToken': '{{csrf_token}}'
+        },
+        data:{
+            access_key: access_key,
+            secret_key: secret_key,
+            region_name: region_name,
+            isRunning: on_off
+        },
+        type:'post',
+        datatype: 'json'
+    }).done(function(response){
+        console.log('status: ' + response.isRunning)
+        console.log('container id: ' + response.containerId)
+
+        if (response.isCreate == 1){
+            alert('Collection Started Successfully \n' + response.containerId)
+        } else if (response.isRunning == 1){
+            alert('Collection Is Already Running!\n' + response.containerId)
+            if (on_off == 1){
+                $(e).attr('class', 'btn btn-md btn-teiren')
+            } else {
+                $(e).attr('class', 'btn btn-md btn-outline-teiren')
+            }
+        } else{
+            alert('Unknown Alert')
+        }
+    })
+}
 
 $('#integration_side').addClass('active');
