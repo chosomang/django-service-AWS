242d4de85468d2f6fdd99039620b48b3c7219780
chosomang <chosomang12@gmail.com>
2023-11-26 05:57:23 +0900

    fix: Modified url patterns

--- django/M_equipments/urls.py
+++ django/M_equipments/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Integration
+    # Integration Page
     path('<pageType>/', views.integration_view),
+
+    ## Configuration Ajax
+    path('configuration/config/<actionType>/', views.integration_config_ajax),
+
+    ## Registration Page/Ajax
     path('<equipment>/<logType>/', views.registration_page),
-    path("<equipment>/<logType>/check/", views.integration_check_ajax),
-    path("<equipment>/<logType>/insert/", views.integration_insert_ajax),
-    path("<equipment>/<logType>/collection/", views.integration_collection_ajax),
-    path('configuration/delete/<actionType>/', views.integration_delete_ajax),
-    path('configuration/trigger/<actionType>/', views.integration_trigger_ajax),
+    path("<equipment>/<logType>/<actionType>/", views.integration_registration_ajax),
 ]
