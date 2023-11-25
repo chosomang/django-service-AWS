fac6628224a0fe35b15b7914c36ce38dfe90f5e3
chosomang <chosomang12@gmail.com>
2023-11-26 06:36:29 +0900

    fix: Modified function corresponding to url

--- django/M_equipments/views.py
+++ django/M_equipments/views.py
@@ -11,33 +11,29 @@ def integration_view(request, pageType):
         return render(request, f"M_equipment/{pageType}.html", context)
     return render(request, f"M_equipment/{pageType}.html")
 
+def integration_config_ajax(request, actionType):
+    if request.method == 'POST':
+        data = dict(request.POST.items())
+        if actionType == 'modal':
+            return render(request, 'M_equipment/configuration/modal.html', data)
+        elif actionType == 'check':
+            return 
+        elif actionType == 'delete':
+            context = integration.delete_integration(data)
+        elif actionType == 'trigger':
+            context = integration.container_trigger(data)
+        return JsonResponse(context)
+
 @login_required
 def registration_page(request, equipment, logType):
     context = {'logType': logType}
     return render(request, f"M_equipment/registration/{equipment}.html", context)
 
-def integration_check_ajax(request, equipment, logType):
+def integration_registration_ajax(request, equipment, logType, actionType):
     if request.method == 'POST':
-        context = integration.integration_check(request, equipment, logType)
-        return JsonResponse(context)
-
-def integration_insert_ajax(request, equipment, logType):
-    if request.method == 'POST':
-        context = integration.integration_insert(request, equipment)
-        return HttpResponse(context)
-
-def integration_delete_ajax(request, actionType):
-    if request.method == 'POST':
-        data = dict(request.POST.items())
-        if actionType == 'modal':
-            return render(request, 'M_equipment/configuration/delete_modal.html', data)
-        context = integration.delete_integration(data)
-        return HttpResponse(context)
-
-def integration_trigger_ajax(request, actionType):
-    if request.method == 'POST':
-        data = dict(request.POST.items())
-        if actionType == 'modal':
-            return render(request, 'M_equipment/configuration/trigger_modal.html', data)
-        context = integration.container_trigger(data)
-        return JsonResponse(context)
+        if actionType == 'check':
+            context = integration.integration_check(request, equipment, logType)
+            return JsonResponse(context)
+        elif actionType == 'insert':
+            context = integration.integration_insert(request, equipment)
+            return HttpResponse(context)
