bd9566042ef815f68bb87df414282e1d691397e7
chosomang <chosomang12@gmail.com>
2023-12-13 17:03:14 +0900

    fix: Modified order of login process

--- django/auth/src/authentication.py
+++ django/auth/src/authentication.py
@@ -23,12 +23,12 @@ def login_account(request, srcip):
         if 1 == graph.evaluate(f"{cypher} RETURN COUNT(a)"):
             if 5 <= graph.evaluate(f"{cypher} RETURN a.failCount"):
                 return 'fail', 'User Id Forbidden. Please Contact Administrator'
-            if 1 == graph.evaluate(f"{cypher} WHERE a.userPassword = '{hashlib.sha256(request['user_password'].encode()).hexdigest()}' RETURN COUNT(a)"):
+            elif 1 != graph.evaluate(f"{cypher} WHERE a.userPassword = '{hashlib.sha256(request['user_password'].encode()).hexdigest()}' RETURN COUNT(a)"):
+                return login_fail(request['user_name'], srcip), 'fail'
+            else:
                 login_success(request['user_name'], srcip)
                 userName = graph.evaluate(f"{cypher} RETURN a.userName")
                 return userName, request['user_password']
-            else:
-                return login_fail(request['user_name'], srcip), 'fail'
         else:
             raise Exception
     except Exception:
