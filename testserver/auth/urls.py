5e95dc2e70d476269c42a66843a5a5ddc5396a98
magaretz <alswn81616@gmail.com>
2024-01-20 18:02:14 +0000

    fixed functions name

--- testserver/auth/urls.py
+++ testserver/auth/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
+    path('login/', views.login_, name='login'),
+    path('logout/', views.logout_, name='logout'),
+    path('register/', views.register_, name='register'),
+    path('activate/<str:uidb64>/<str:token>/', views.activate_, name='activate'),
 ]
