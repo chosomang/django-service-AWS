7340348963f8ade81676aa441ed2706ddf941a89
magarets <alswn81616@gmail.com>
2024-01-23 06:55:28 +0000

    fixed neo4j executer

--- testserver/common/neo4j/handler.py
+++ testserver/common/neo4j/handler.py
@@ -1,3 +1,5 @@
+# local
+from datetime import datetime
 # 3rd party
 from neo4j import GraphDatabase
 # django
@@ -20,16 +22,47 @@ class Neo4jHandler:
     
     def run(self, database, query):
         with self.driver.session(database=database) as session:
-            res = session.write_transaction(self._run, query)
+            res = session.execute_write(self._run, query)
             return res
     
-    def create_database(self, database):
-        query = f"CREATE DATABASE `{database}`"
-        with self.driver.session(database=database) as session:
-            res = session.write_transaction(self._run, query)
-            return res
+    def create_database(self, db_name):
+        query = f"CREATE DATABASE `{db_name}`"
+        with self.driver.session() as session:
+            session.execute_write(self._run, query)
+        self.close()
     
     @staticmethod
-    def _run(tx, database, query):
+    def _run(tx, query):
         res = tx.run(query)
         return res
+    
+# neo4j_handler = Neo4jHandler()
+
+
+class Cypher(Neo4jHandler):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def close(self):
+        Neo4jHandler.close(self)
+        
+    def push_user(self, user, ip):
+        query = f"""
+MERGE (super:Super:Teiren {{name:'Teiren'}})
+WITH super
+MERGE (super)-[:SUB]->(a:Teiren:Account {{
+    userName: '{user.username}',
+    userPassword: '{user.password}',
+    email: '{user.email}',
+    createdTime: '{str(datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'))}',
+    ipAddress: '{ip}',
+    uuid: '{user.uuid}',
+    failCount: 0,
+    emailCheck: 'False'
+}})
+RETURN ID(a) as id
+"""
+        res = Neo4jHandler.run(self, user.db_name, query)
+        return res
+        
+# cypherhandler = Cypher()
