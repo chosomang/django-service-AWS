09da84ba3187a4a968365f3e1e15b61a6c27ae6c
magarets <alswn81616@gmail.com>
2024-01-24 13:29:04 +0000

    added user dashboard layout field

--- testserver/_auth/models.py
+++ testserver/_auth/models.py
@@ -5,5 +5,6 @@ import uuid
 class CustomUser(AbstractUser):
     uuid = models.UUIDField(default=str(uuid.uuid4()), unique=True)
     email = models.EmailField(unique=True)  # 이메일 필드 추가
+    user_layout = models.CharField(max_length=50)
     db_name = models.CharField(max_length=50, default=False, unique=True)
     is_active = models.BooleanField(default=False)
