65a0252c474ee7a4b4c55c31b63ae29ef79c5138
magarets <alswn81616@gmail.com>
2024-01-26 20:56:27 +0000

    feat: apply class to views

--- testserver/M_logs/views.py
+++ testserver/M_logs/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
-from .src import log
+from .src.log import DashboardLogHandler
 
 # Log Management
 @login_required
 def log_view(request, resourceType, logType):
     if request.method == 'POST':
-        context = dict(request.POST)
-        context = (log.get_log_page(context, logType.split(' ')[0]))
-        return render(request,f"M_logs/dataTable.html",context)
-    context = dict(request.GET.items())
-    context = (log.get_log_page(context, logType.split('_')[0]))
-    if resourceType == 'cloud':
-        context['logType']= logType.upper()
-    else:
-        logType = (' ').join(logType.split('_'))
-        context['logType']= logType.title()
-    context['resource'] = resourceType
-    return render(request,f"M_logs/log.html",context)
+        with DashboardLogHandler(request=request) as lhandler:
+            context = (lhandler.get_log_page(logType.split(' ')[0]))
+            return render(request,f"M_logs/dataTable.html",context)
+    
+    with DashboardLogHandler(request=request) as lhandler:
+        context = (lhandler.get_log_page(logType.split('_')[0]))
+        context['resource'] = resourceType
+        
+        if resourceType == 'cloud':
+            context['logType']= logType.upper()
+        else:
+            logType = (' ').join(logType.split('_'))
+            context['logType']= logType.title()
+            
+        return render(request,f"M_logs/log.html",context)
 
 ## Detail Modal
 def log_modal(request, resourceType, logType):
@@ -25,5 +28,6 @@ def log_modal(request, resourceType, logType):
         if logType == 'help':
             return render(request, "M_logs/help.html")
         elif logType == 'details':
-            context = log.get_log_detail_modal(dict(request.POST.items()))
+            with DashboardLogHandler(request=request) as lhandler:
+                context = lhandler.get_log_detail_modal(dict(request.POST.items()))
             return render(request, "M_logs/detail_modal.html", context)
