23d4306942965e2a5dff542ed845a9cc479518c1
magarets <alswn81616@gmail.com>
2024-01-30 17:11:48 +0000

    Correct incorrect grammar

--- testserver/compliance/src/file.py
+++ testserver/compliance/src/file.py
@@ -4,27 +4,31 @@ import docx2pdf
 from aspose.slides import Presentation
 
 def get_file_preivew_details(request, evidence_type):
+    title = request.POST.get('comment', '')
+    user_uuid = request.session.get('uuid')
+    
     # user_uuid = request.session.get('uuid')
     if evidence_type == 'policy':
-        documents = Policy.objects.filter(title=request.POST.get('comment', ''))
+        documents = Policy.objects.filter(user_uuid=user_uuid, title=title)
     elif evidence_type == 'asset':
-        documents = Asset.objects.filter(title=request.POST.get('comment', ''))
+        documents = Asset.objects.filter(user_uuid=user_uuid, title=title)
     else:
-        documents = Evidence.objects.filter(title=request.POST.get('comment', ''), product=evidence_type)
+        documents = Evidence.objects.filter(user_uuid=user_uuid, title=title, product=evidence_type)
+        
     for document in documents:
-        if document.uploadedFile.name.endswith(request.POST.get('name', '').replace('[','').replace(']','')):
+        name = request.POST.get('name', '')
+        if document.uploadedFile.name.endswith(name.replace('[','').replace(']','')):
             mime_type, _ = mimetypes.guess_type(document.uploadedFile.path)
             file_url = document.uploadedFile.url
             file_path = document.uploadedFile.path
-            if mime_type is None:
-                mime_type = 'image/png'
+            if mime_type == 'image/png' or mime_type == 'image/jpg' or mime_type == 'image/jpeg':
+                print(f"mime_type: {mime_type}")
+                return '/home/yoonan/webAPP/testserver'+file_url, mime_type
             elif 'docx' in mime_type:
                 file_url, mime_type = docx_to_pdf(file_path, file_url)
             elif 'ppt' in mime_type:
                 file_url, mime_type = ppt_to_img(file_path, file_url)
-    print(file_url)
-    print(mime_type)
-    return file_url, mime_type
+    return '/home/yoonan/webAPP/testserver'+file_url, mime_type
 
 def docx_to_pdf(file_path, file_url):
     preview_path = '\\'.join(file_path.split('\\')[:-1])+'\\preview.pdf'
