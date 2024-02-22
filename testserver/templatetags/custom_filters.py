650ad92c65cf2ddad9080d7069980493d5086884
magarets <alswn81616@gmail.com>
2024-02-22 15:34:15 +0000

    custom filter for django template

--- testserver/templatetags/custom_filters.py
+++ testserver/templatetags/custom_filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 def replace(value, arg):
     original, new = arg.split(',')
     return value.replace(original, new)
+
+@register.filter(name='has_image_extension')
+def has_image_extension(value):
+    image_extensions = ['.jpg', '.jpeg', '.png']
+    return any(value.lower().endswith(ext) for ext in image_extensions)
