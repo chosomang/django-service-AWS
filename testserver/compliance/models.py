65260244e192b56e5efa40f555964e90ba6812f1
magarets <alswn81616@gmail.com>
2024-02-22 15:25:51 +0000

    Add custom storage filter class

--- testserver/compliance/models.py
+++ testserver/compliance/models.py
@@ -1,5 +1,8 @@
+from custom.storage.customstorage import CleanFileNameStorage
+
 from django.db import models
 from django.utils.text import slugify
+
 
 def evidence_directory_path(instance, filename):
     return f'{instance.user_uuid}/Evidence/{slugify(instance.product)}/{filename}'
@@ -14,19 +17,22 @@ class Evidence(models.Model):
     user_uuid = models.CharField(max_length=50)
     title = models.CharField(max_length=200)
     product = models.CharField(max_length=500)
-    uploadedFile = models.FileField(upload_to=evidence_directory_path)
+    uploadedFile = models.FileField(upload_to=evidence_directory_path, 
+                                    storage=CleanFileNameStorage())
     dateTimeOfUpload = models.DateTimeField(auto_now=True)
 
 
 class Policy(models.Model):
     user_uuid = models.CharField(max_length=50)
     title = models.CharField(max_length=200)
-    uploadedFile = models.FileField(upload_to=policy_directory_path)
+    uploadedFile = models.FileField(upload_to=policy_directory_path,
+                                    storage=CleanFileNameStorage())
     dateTimeOfUpload = models.DateTimeField(auto_now=True)
 
 
 class Asset(models.Model):
     user_uuid = models.CharField(max_length=50)
     title = models.CharField(max_length=200)
-    uploadedFile = models.FileField(upload_to=asset_directory_path) # /
+    uploadedFile = models.FileField(upload_to=asset_directory_path,
+                                    storage=CleanFileNameStorage()) # /
     dateTimeOfUpload = models.DateTimeField(auto_now=True)
